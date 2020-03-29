89c7694f48ad527a56649a8beeb91731e47d23da
13020180060 <61998954+13020180060@users.noreply.github.com>
2020-03-28 23:16:40 -0700

    Add files via upload

---
+++ operatorTernary.java
@@ -0,0 +1,12 @@
+public class operatorTernary {
+public static void main(String[] args) {
+
+boolean suka = true;
+String jawaban;
+
+jawaban = suka ? "iya" : "tidak";
+
+System.out.println(jawaban);
+
+}
+}
